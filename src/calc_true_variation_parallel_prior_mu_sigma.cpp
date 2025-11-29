0f12d1014bf55391999b9a9677f8f8c1bcab4992
dhdegroot <daanhugodegroot@gmail.com>
2025-11-29 11:09:48 +0100

    Update calc_true_variation_parallel_prior_mu_sigma.cpp

--- src/calc_true_variation_parallel_prior_mu_sigma.cpp
+++ src/calc_true_variation_parallel_prior_mu_sigma.cpp
@@ -821,8 +821,8 @@ void parse_argv(int argc,char** argv, string &in_file, string &gene_name_file, s
 				if(j==5) vmax = atof(argv[idx+1]);
 				if(j==6) numbin = atoi(argv[idx+1]);
 				if(j==7) gene_name_file = argv[idx+1];
-				// if(j==8) cell_name_file = argv[idx+1];
-				// if(j==9) no_norm_str = argv[idx+1];
+				if(j==8) cell_name_file = argv[idx+1];
+				if(j==9) no_norm_str = argv[idx+1];
 				// if(j==10) max_v_str = argv[idx+1];
 				// // add '/' to out_folder if not already
 				// if( j == 1 && out_folder.back() != '/' )
